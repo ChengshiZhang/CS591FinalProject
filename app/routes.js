546de620273dd14a475a4e539f4c0335809ef45f
ChengshiZhang <chengshizhang09@gmail.com>
2017-06-25 08:17:48 -0400

    Implemented basic file upload

--- app/routes.js
+++ app/routes.js
@@ -21,6 +21,24 @@ module.exports = function(app) {
         });
     });
 
+    app.post('/api/upload', function(req, res) {
+        if (!req.files)
+            return res.status(400).send('No files were uploaded.');
+
+        // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+        let sampleFile = req.files.sampleFile;
+        
+        console.log(req.files.sampleFile);
+
+        // Use the mv() method to place the file somewhere on your server
+        sampleFile.mv('uploads/pic1.jpg', function(err) {
+            if (err)
+                return res.status(500).send(err);
+
+            res.send('File uploaded!');
+        });
+    });
+
     app.post('/api/NewAlbum_Upload', function(req, res) {
         //res.sendfile('./public/views/NewAlbum_Upload.html');
         res.json(req.body);
